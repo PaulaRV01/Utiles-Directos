5c40f044a8887367d727b7b1d467d3bd25b1819a
Rojas_Villanueva_Paula <paulaelianne.rojas@unmsm.edu.pe>
2024-02-27 22:10:23 -0500

    update conexion.php

--- DATOS/conexion/conexion.php
+++ DATOS/conexion/conexion.php
@@ -1,19 +1,31 @@
 <?php
+$host = "srvmysql.mysql.database.azure.com";
+$user = "srvadmin";
+$clave = "#utilesdirectos1*";
+$bd = "sis_venta";
 
-    $host = "srvmysql.mysql.database.azure.com";
-    $user = "srvadmin";
-    $clave = "#utilesdirectos1*";
-    $bd = "sis_venta";
+// Inicializar la conexión mysqli
+$conexion = mysqli_init();
 
-    $conexion = mysqli_connect($host,$user,$clave,$bd);
-    if (mysqli_connect_errno()){
-        echo "No se pudo conectar a la base de datos";
-        exit();
-    }
+// Configurar SSL
+mysqli_ssl_set($conexion, NULL, NULL, "C:\Users\Paula\Downloads\DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 
-    mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos");
+// Realizar la conexión utilizando SSL
+mysqli_real_connect($conexion, $host, $user, $clave, $bd, 3306, MYSQLI_CLIENT_SSL);
 
-    mysqli_set_charset($conexion,"utf8");
+// Verificar la conexión
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
 
+// Establecer el charset
+mysqli_set_charset($conexion, "utf8");
 
+// Ahora puedes usar la conexión $conn para realizar consultas SQL
+// Por ejemplo:
+//$query = "SELECT * FROM tabla";
+//$result = mysqli_query($conn, $query);
+//while ($row = mysqli_fetch_assoc($result)) {
+//    echo $row['columna'];
+//}
 ?>
